e04867cd279c7dc1d6ed5cde9cfb8e553c6a5802
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-06-13 22:54:03 -0600

    Updated the routes and changed the port to 8080

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app
   .use(bodyParser.json())
